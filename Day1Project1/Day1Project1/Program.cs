4428dec2ff3a48716caa61611bcc78c85d7279e7
meganadh <meganadh@gmail.com>
2022-01-25 08:37:02 +0530

    day 2 progress

---
+++ Day1Project1/Day1Project1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float data = 6.0123456789232434343554645657f;
+
+            double data2 = 6.0123456789232434343554645657d;
+
+            decimal data3 = 6.0123456789232434343554645657m;
+
+            Console.WriteLine(data);
+
+            Console.WriteLine(data2);
+
+            Console.WriteLine(data3);
+
+            Console.ReadLine();
+        }
+    }
+}
